93cb69d97ad79c782de423d8f16623e3869900fa
RuiFerreira05 <ruimf.05@gmail.com>
2026-05-02 16:51:38 +0100

    Safe padding around weatherUI

--- app/src/main/java/dam_a51597/coolweatherappv2/MainActivity.kt
+++ app/src/main/java/dam_a51597/coolweatherappv2/MainActivity.kt
@@ -4,6 +4,9 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.safeDrawingPadding
+import androidx.compose.ui.Modifier
 import dam_a51597.coolweatherappv2.ui.WeatherUI
 
 class MainActivity : ComponentActivity() {
@@ -11,7 +14,11 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            WeatherUI()
+            Box(
+                Modifier.safeDrawingPadding()
+            ) {
+                WeatherUI()
+            }
         }
     }
 }
